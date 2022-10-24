72b57666cd75fc60b921982d24b9f40e679267f2
desire barine <desirebarine16@gmail.com>
2022-10-24 16:16:43 +0000

    first commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for val in all_cls.values():
+            return val
+        return None
+
+    def 
