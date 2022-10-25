99c04a6eefdf46c6b9b69fd5cca9529402978bd9
desire barine <desirebarine16@gmail.com>
2022-10-25 06:53:10 +0000

    task 1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,29 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for val in all_cls.values():
+            return val
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class
+        If no class is passed, returns the count of all objects in storage
+        """
+        all_clss = classes.values()
+
+        if not cls:
+            count = 0
+            for clss in all_clss:
+                count += len(models.storage.all(clss).values())
+        else:
+            count = len(models.storage.all(cls).values())
+        return count
