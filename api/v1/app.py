3bac29d60ec5a40ee2a198022f11850f25a255c9
Dessira <99396824+Dessira@users.noreply.github.com>
2022-12-15 10:26:48 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,29 @@
-#!/flask/bin/python
-"""Flask app """
-
-from flask import Flask
-from models import storage
-from app.v1.views import app_views
+#!/usr/bin/python3
+"""app.py to connect to API"""
 import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-@app.teardown_appcontexta
-def teardown(exception):
-    """close storage"""
-    if storage is not None:
-        storage.close()
+
+@app.teardown_appcontext
+def teardown_appcontext(code):
+    """teardown_appcontext"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))ded=True)
 
